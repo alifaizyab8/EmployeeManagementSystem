ffb8886ee104faf3825cbdaacb472aa923d8cd5d
waqarwasif <waqarwasif0333@gmail.com>
2025-11-18 16:45:31 +0500

    tolower and nameformat added

--- EMS/app/libraryfunc.c
+++ EMS/app/libraryfunc.c
@@ -12,15 +12,72 @@ int strlen_custom(const char *str)
 
 int strcmp_custom(const char *str1, const char *str2)
 {
+
     while (*str1 && (*str1 == *str2))
+
     {
+
         str1++;
         str2++;
+
     }
+
     return *(const unsigned char *)str1 - *(const unsigned char *)str2;
 }
 
+
 int isalpha_custom(char c)
 {
+
     return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c == ' '));
+
 }
+
+
+
+void toLowercase_custom(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] + ('a' - 'A');
+        }
+    }
+}
+
+
+void NameFormat_custom(char *str)
+{
+    toLowercase_custom(str);
+    while(1){
+        
+        if (str[0] == ' ')
+        {
+            for (int i = 0; str[i] != '\0'; i++)
+            {
+                str[i] = str[i + 1];
+            }
+
+        }
+        else{
+            break;
+        }
+    }
+        
+    if (str[0] >= 'a' && str[0] <= 'z')
+    {
+        str[0] = str[0] - ('a' - 'A');
+    }
+
+    for (int i = 1; str[i] != '\0'; i++)
+
+    {
+        if (str[i - 1] == ' ' && (str[i] >= 'a' && str[i] <= 'z'))
+        {
+            str[i] = str[i] - ('a' - 'A');
+        }
+    }
+
+}
+

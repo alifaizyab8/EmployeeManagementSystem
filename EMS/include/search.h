bf201571448d2da4b59fa33ee19ea59e0aafef7f
waqarwasif <waqarwasif0333@gmail.com>
2025-11-18 12:52:15 +0500

    search declarations added

--- EMS/include/search.h
+++ EMS/include/search.h
@@ -1,7 +1,11 @@
 //Search Menu Funcitons header file
 #ifndef SEARCH_H
 #define SEARCH_H
+#include "employee.h"
 
-
+void searchbyID(struct Employee employees[], int size);
+void searchbyName(struct Employee employees[], int size);
+void searchbyAge(struct Employee employees[], int size);
+void searchbyDepartment(struct Employee employees[], int size);
 
 #endif

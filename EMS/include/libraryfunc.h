b281bdc9da739656ce2c4cb6576c40422f3c7692
waqarwasif <waqarwasif0333@gmail.com>
2025-11-18 20:36:10 +0500

    edited

--- EMS/include/libraryfunc.h
+++ EMS/include/libraryfunc.h
@@ -6,6 +6,6 @@ int strcmp_custom(const char *str1, const char *str2);
 int isalpha_custom(char c);
 void toLowercase_custom(char *str);
 void NameFormat_custom(char *str);
-void strcpy_custom(char *dest, char *src);
+void strcpy_custom(char *dest, const char *src);
 
 #endif

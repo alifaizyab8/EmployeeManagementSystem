c96df6788b383438310d3f5e072fa30e583c56b5
waqarwasif <waqarwasif0333@gmail.com>
2025-11-18 10:49:03 +0500

    isalpha added

--- EMS/app/libraryfunc.c
+++ EMS/app/libraryfunc.c
@@ -19,3 +19,8 @@ int strcmp_custom(const char *str1, const char *str2)
     }
     return *(const unsigned char *)str1 - *(const unsigned char *)str2;
 }
+
+int isalpha_custom(char c)
+{
+    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c == ' '));
+}

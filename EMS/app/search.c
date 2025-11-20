57975ba2756598d33a4ce5b029f7c306127fabd6
waqarwasif <waqarwasif0333@gmail.com>
2025-11-20 19:59:49 +0500

    added search by position option

--- EMS/app/search.c
+++ EMS/app/search.c
@@ -216,3 +216,77 @@ void searchbyAge(struct Employee employees[], int size)
 
     line();
 }
+
+
+void searchbyposition(struct Employee employees[], int size)
+{
+
+    system("cls");
+    char pos[50];
+    
+    printf("Enter Employee Position: ");
+    scanf(" %49[^\n]", pos);
+    while (getchar() != '\n');
+
+    toLowercase_custom(pos);
+    line();
+
+    int found = 0;
+    for(int i = 0; i < size; i++)
+    {   
+        char temppos[50];
+        strcpy_custom(temppos, employees[i].position);
+
+        toLowercase_custom(temppos);
+        if(strcmp_custom(temppos, pos)==0)
+        {
+
+            found = 1;
+            break;
+
+        }
+    }
+        if(!found)
+        {
+
+            printf("No employee found in the department: %s\n", pos);
+            line();
+            return;
+
+        }
+
+        else{
+
+            printf("\033[1;33m%-5s %-15s %-15s %-5s %-15s %-10s %-12s %-10s %-10s\033[0m\n",
+                   "ID", "First Name", "Last Name", "Age", "Position", "Salary", "Working Hrs", "Overtime", "Rating");
+            line();
+
+            for(int i = 0; i < size; i++)
+            {
+                char temppos2[50];
+                strcpy_custom(temppos2, employees[i].position);
+
+                toLowercase_custom(temppos2);
+
+                if(strcmp_custom(temppos2, pos)==0)
+                {
+
+                    printf("%-5d %-15s %-15s %-5d %-15s %-10.2f %-12d %-10d %-10.2f\n",
+                           employees[i].id,
+                           employees[i].emp.firstname,
+                           employees[i].emp.lastname,
+                           employees[i].age,
+                           employees[i].position,
+                           employees[i].salary,
+                           employees[i].working_hours,
+                           employees[i].over_time,
+                           employees[i].performance_rating);
+                }
+            }
+        }
+
+    
+    
+    line();
+
+}

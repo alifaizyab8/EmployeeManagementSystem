65db870c17abd794cba4eb318f80c09bc986c995
waqarwasif <waqarwasif0333@gmail.com>
2025-11-21 21:23:07 +0500

    added comments to main.c file

--- EMS/app/main.c
+++ EMS/app/main.c
@@ -1,18 +1,29 @@
+// Necessary header files included
 #include <stdio.h>
 #include "../include/employee.h"
 #include "../include/login.h"
 #include "../include/menuHandler.h"
 #include "../include/search.h"
+
 int main()
 {
-        // displayLoginScreen();
-        // Very Important Lines below
+        // Display the login screen and authenticate the user
+        // Program will continue only after successful login
+        displayLoginScreen();
+
+        // Initialize an array of Employee structures to store all employee data
+        // MAX_EMPLOYEES ensures we don't exceed the allocated size
         struct Employee employees[MAX_EMPLOYEES];
+
+         // Load employee records from storage (file/database)
+        // Returns the number of employees successfully loaded
         int employeeCount = loadEmployee(employees, MAX_EMPLOYEES);
 
+        // Display and handle the main menu options
+        // Pass employee data and count so menu options can modify them
         showMenu(employees, &employeeCount);
 
-        return 0;
+        return 0; // End of program
 }
 /*
 To run the program, compile all the .c files together and execute the resulting binary.

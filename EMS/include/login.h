326cea55cd99c5e5e1704e97b73d92fd0ad66860
waqarwasif <waqarwasif0333@gmail.com>
2025-11-21 21:29:24 +0500

    added comments to login.h file

--- EMS/include/login.h
+++ EMS/include/login.h
@@ -1,7 +1,10 @@
 #ifndef login_h
 #define login_h
 
-void displayLoginScreen();
-void line();
+void displayLoginScreen(); // Function to display the login screen and handle user authentication
+void line(); // Function to print a line separator
+void printEMS(); // Function to print EMS in stylized text format
+void printLoginTitle(); // Function to print Login Title in stylized text format
+
 
 #endif

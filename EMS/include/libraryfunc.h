60559c855b5d33293f62a5cdd7323f3041ad61cd
waqarwasif <waqarwasif0333@gmail.com>
2025-11-21 21:27:26 +0500

    added comments to libraryfunc.h file

--- EMS/include/libraryfunc.h
+++ EMS/include/libraryfunc.h
@@ -1,11 +1,11 @@
 #ifndef LIBRARYFUNC_H
 #define LIBRARYFUNC_H
 
-int strlen_custom(const char *str);
-int strcmp_custom(const char *str1, const char *str2);
-int isalpha_custom(char c);
-void toLowercase_custom(char *str);
-void NameFormat_custom(char *str);
-void strcpy_custom(char *dest, const char *src);
+int strlen_custom(const char *str);  // Get length of a string
+int strcmp_custom(const char *str1, const char *str2); // Compare two strings
+int isalpha_custom(char c); // Check if character is alphabet letter or space
+void toLowercase_custom(char *str); // Convert string to lowercase
+void NameFormat_custom(char *str); // Format name string (capitalize first letters and remove leading spaces)
+void strcpy_custom(char *dest, const char *src); // copy string from src to dest
 
 #endif

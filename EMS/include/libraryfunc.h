1e597f01008d0c1872c9241fc5f8a7a5b187455f
waqarwasif <waqarwasif0333@gmail.com>
2025-11-22 16:41:43 +0500

    added atoi and isdigit declarations

--- EMS/include/libraryfunc.h
+++ EMS/include/libraryfunc.h
@@ -7,5 +7,7 @@ int isalpha_custom(char c); // Check if character is alphabet letter or space
 void toLowercase_custom(char *str); // Convert string to lowercase
 void NameFormat_custom(char *str); // Format name string (capitalize first letters and remove leading spaces)
 void strcpy_custom(char *dest, const char *src); // copy string from src to dest
+int isDigitString_custom(const char *str); // Check if string contains only digit characters (0-9)
+int atoi_custom(const char *str); // Convert string to integer
 
 #endif
